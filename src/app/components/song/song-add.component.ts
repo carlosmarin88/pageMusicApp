d7d2e796850faf46f2d27455e31ed356314fe8b2
Carlos marin <carlosmarin151288@gmail.com>
2017-12-14 20:31:22 -0300

    Se sube el proceso de canciones

--- src/app/components/song/song-add.component.ts
+++ src/app/components/song/song-add.component.ts
@@ -49,7 +49,7 @@ export class SongAddComponent implements OnInit {
                     }else{
                         this.alertMessage = 'El fichero se ha creado correctamente!';
                         this.song = response.song;
-                        //this._router.navigate(['/']);
+                        this._router.navigate(['/editar-tema',response.song._id]);
                     }
                 },
                 error=>{
